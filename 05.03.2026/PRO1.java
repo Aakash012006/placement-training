817fa16b11251a62a03155f4b64fb2964439d428
Aakash012006 <151338191+Aakash012006@users.noreply.github.com>
2026-03-05 17:42:59 +0530

    Create PRO1.java

---
+++ 05.03.2026/PRO1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        
+        
+        System.out.println(A.length() + B.length());
+        
+       
+        if (A.compareTo(B) > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        
+       
+        String capA = Character.toUpperCase(A.charAt(0)) + A.substring(1);
+        String capB = Character.toUpperCase(B.charAt(0)) + B.substring(1);
+        System.out.println(capA + " " + capB);
+        sc.close();
+    }
+}
